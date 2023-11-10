74721289d2dc7c10e109985611688476d7b64c4c
G. Yang <107653576+gyang0@users.noreply.github.com>
2023-11-10 08:52:40 -0800

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,11 +1,19 @@
 class Block {
-	constructor(){
-
+	constructor(x, y, w, h, imgSrc){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.imgSrc = imgSrc;
 	}
 
-	display(){}
+	display(){
+		ctx.drawImage(images[this.imgSrc], this.x, this.y, this.w, this.h);
+	}
 
-	update(){}
+	update(){
+
+	}
 }
 
 class SolidBlock extends Block {}
@@ -18,6 +26,23 @@ class DecorBlock extends Block {}
 
 class Enemy {}
 
-class Player {}
+class Player extends Block {
+	constructor(x, y, w, h, imgSrc){
+		super(x, y, w, h, imgSrc);
+
+		this.speed = 5;
+		this.gravity = 4.8;
+	}
+
+	update(){
+		if(keys.w || keys.W || keys.ArrowUp) this.y -= this.speed;
+		if(keys.a || keys.A || keys.ArrowLeft) this.x -= this.speed;
+		if(keys.s || keys.S || keys.ArrowDown) this.y += this.speed;
+		if(keys.d || keys.D || keys.ArrowRight) this.x += this.speed;
+
+		//this.y += this.gravity;
+		this.y = Math.min(this.y, HEIGHT - 400);
+	}
+}
 
 class Rocket {}
