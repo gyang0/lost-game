0fbf76315d0ce3c7b8d5071b42c8ac1a2121ace0
G. Yang <107653576+gyang0@users.noreply.github.com>
2023-11-11 02:20:49 -0800

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,24 +1,32 @@
 class Slideshow {
-	constructor(scenes, interval){
+	constructor(scenes, durations, x, y, w, h){
 		this.scenes = scenes; // List of scenes (images)
-		this.interval = interval; // milliseconds of pause between scenes
-
+		this.durations = durations; // Duration (milliseconds) for each scene
+		
 		this.index = 0;
 
-		this.x = 0;
-		this.y = 0;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+
+		this.timer = 0;
 	}
-	run(frameCount, callback){
-		// Framecount = 0 case must be ignored to avoid instantaneous switches
-		if(frameCount > 0 && frameCount % this.interval == 0){
+	run(callback){
+		// WARNING: framecount = 0 case must be ignored to avoid instantaneous switches
+		this.timer++;
+
+		if(this.timer >= this.durations[this.index]){
 			this.index++;
 
 			if(this.index >= this.scenes.length){
 				callback();
 				this.index = this.scenes.length - 1;
 			}
+
+			this.timer = 0;
 		}
 
-		ctx.drawImage(images[this.scenes[this.index]], WIDTH/2, HEIGHT/2, 100, 100);
+		ctx.drawImage(images[this.scenes[this.index]], this.x, this.y, this.w, this.h);
 	}
 }
